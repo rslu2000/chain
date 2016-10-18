6acc3c77fbd63c44596cf68c8378315121b01be8
Zarya Faraj <zaryafaraj@gmail.com>
2016-10-17 17:59:41 -0700

    new layout for protocol page

--- docs/js/layout.js
+++ docs/js/layout.js
@@ -13,35 +13,6 @@ $(function () {
 
 
 
-// Modal to sign up for newsletter
-$(function () {
-  var modal = document.getElementById('downloadModal');
-
-// Get the button that opens the modal
-  var btn = document.getElementById("downloadBtn");
-
-// Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-  btn.onclick = function () {
-    modal.style.display = "block";
-  }
-
-// When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  }
-
-// When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-})
-
-
 
 // switcher between the navtabs for operating systems
 function openOS(evt, osName) {
@@ -82,10 +53,55 @@ $("#linuxRequest").click(function() {
 
 
 // Find all content between h2 and wrap it with section tag
-
-
 $('#doc-content h2').each(function(){
   $(this).nextUntil('h2').andSelf().wrapAll('<section />');
   $(this).nextAll().wrapAll()
     .end().wrap();
 });
+
+
+
+// Modal to sign up for newsletter
+function showSignUpForm() {
+  var modal = document.getElementById('downloadModal');
+
+// Get the button that opens the modal
+  var btn = document.getElementById("downloadBtn");
+
+// Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+  modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+$(function () {
+  $("#downloadBtn1").click(function() {
+    showSignUpForm();
+    return true;
+  });
+})
+
+
+//$(function () {
+//  var biggestHeight = "0";
+//  $("section *").each(function () {
+//    if ($(this).height() > biggestHeight) {
+//      biggestHeight = $(this).height()
+//    }
+//  });
+//
+//
+//  $("section").height(biggestHeight);
+//});
