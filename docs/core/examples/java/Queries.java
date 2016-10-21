6825d42c4662a7fec690f35ec030b3860f67493d
Devon Gundry <devon@chain.com>
2016-10-21 12:59:27 -0700

    update query guide

--- docs/core/examples/java/Queries.java
+++ docs/core/examples/java/Queries.java
@@ -21,5 +21,33 @@ class Queries {
       .addFilterParameter("yes")
       .execute(client);
     // endsnippet
+
+    // snippet list-local-assets
+    Asset.Items localAssets = new Asset.QueryBuilder()
+      .setFilter("is_local=$1")
+      .addFilterParameter("yes")
+      .execute(client);
+    // endsnippet
+
+    // snippet list-usd-assets
+    Asset.Items localAssets = new Asset.QueryBuilder()
+      .setFilter("definition.currency=$1")
+      .addFilterParameter("USD")
+      .execute(client);
+    // endsnippet
+
+    // snippet list-checking-accounts
+    Account.Items checkingAccounts = new Account.QueryBuilder()
+      .setFilter("tags.type=$1")
+      .addFilterParameter("checking")
+      .execute(client);
+    // endsnippet
+
+    // snippet list-alice-unspents
+    UnspentOutput.Items aliceUnspentOuputs = new UnspentOutput.QueryBuilder()
+      .setFilter("account_alias=$1")
+      .addFilterParameter("alice")
+      .execute(client);
+    // endsnippet
   }
 }
